8453eafcca62844c925439b04a825c8de7127e66
Dani Portela <daniel.portela96@hotmail.com>
2023-01-03 20:38:14 -0300

    Create and style Navbar component.

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,13 @@
+import "./App.scss";
 import React from "react";
 
+import { Navbar } from "./components";
 import { Header, About, Projects, Skills, Testimonial, Footer } from "./container";
 
 export default function App() {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <About />
       <Projects />
